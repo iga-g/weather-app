659b6f767002673d868f7544cdb9ec7552db26d7
iga-g <igg@interia.eu>
2021-03-07 20:47:11 +0100

    xx

--- scripts/app.js
+++ scripts/app.js
@@ -16,46 +16,50 @@ const updateUI = (data) => {
   const dayFormat = dateFns.format(new Date(cityDate), 'ddd');
   const dateFormat = dateFns.format(new Date(cityDate), 'DD MMMM YYYY');
 
+  card.classList.remove('visible');
+  card.classList.add('invisible');
+
   // destructure properties
   // const { cityDets, weather } = data;
   // ${cityDate.toDateString()}
 
-  details.innerHTML = `
-  <div>
-  <h6>${dayFormat}</h6>
-  <h6>${dateFormat}</h6>
-  <h5><i class="fas fa-map-marker-alt"></i> ${cityDets.EnglishName}</h5>
-  </div>
-  <div>
-    <span>${weather.Temperature.Metric.Value}</span>
-    <span>&deg;C</span>
-    <p>${weather.WeatherText}</p>
-  </div>
+  setTimeout(function () {
+    card.classList.remove('invisible');
+    card.classList.add('visible');
+
+    details.innerHTML = `
+    <div>
+    <h6>${dayFormat}</h6>
+    <h6>${dateFormat}</h6>
+    <h5><i class="fas fa-map-marker-alt"></i> ${cityDets.EnglishName}</h5>
+    </div>
+    <div>
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+      <p>${weather.WeatherText}</p>
+    </div>
+    
+    `;
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
   
-  `;
+    let timeSrc = null;
+    if (weather.IsDayTime) {
+      timeSrc = 'img/day.svg';
+      body.className = 'day-bg';
+    } else {
+      timeSrc = 'img/night.svg';
+      body.className = 'night-bg';
+      icon.className = 'icon-night';
+    }
+    time.setAttribute('src', timeSrc);
+  
+    if (weather.IsDayTime && icon.classList) {
+      icon.classList.remove('icon-night');
+    }
 
-  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
-  icon.setAttribute('src', iconSrc);
-
-  let timeSrc = null;
-  if (weather.IsDayTime) {
-    timeSrc = 'img/day.svg';
-    body.className = 'day-bg';
-  } else {
-    timeSrc = 'img/night.svg';
-    body.className = 'night-bg';
-    icon.className = 'icon-night';
-  }
-  time.setAttribute('src', timeSrc);
-
-  if (weather.IsDayTime && icon.classList) {
-    icon.classList.remove('icon-night');
-  }
-
-  if (card.classList.contains('d-none')) {
-    card.classList.remove('d-none');
-    // card.classList.add('visible');
-  }
+  }, 500);
   
 };
 
